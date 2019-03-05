c51fe0ea6769194a773c41935d196c40ad4dc288
monjur <monjurmorshed793@gmail.com>
2019-03-05 23:19:03 +0600

    models added.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,26 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'department',
+                loadChildren: './department/department.module#SofplDepartmentModule'
+            },
+            {
+                path: 'designation',
+                loadChildren: './designation/designation.module#SofplDesignationModule'
+            },
+            {
+                path: 'holiday-cat',
+                loadChildren: './holiday-cat/holiday-cat.module#SofplHolidayCatModule'
+            },
+            {
+                path: 'holiday',
+                loadChildren: './holiday/holiday.module#SofplHolidayModule'
+            },
+            {
+                path: 'personal-info',
+                loadChildren: './personal-info/personal-info.module#SofplPersonalInfoModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
